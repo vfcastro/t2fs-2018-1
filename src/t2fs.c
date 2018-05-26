efadf0ebe9b5b9de14a1e089194f3619cbf31112
vfcastro <vinicius_decastro@vinicius>
2018-05-26 02:13:55 -0300

    impl init(), definicao struct HANDLE e CWD

--- src/t2fs.c
+++ src/t2fs.c
@@ -9,19 +9,30 @@
 #define SUCCESS 0
 #define ERROR -1
 
+// Struct do handle de um arquivo/diretorio aberto
 typedef struct {
-	DIR2 handle;
-	struct t2fs_inode inode;
-	
-	
-} OPEN_DIR;
-
+	int id; // identificador
+	struct t2fs_record record; // entrada de diretorio
+	DWORD current_offset; // deslocamento em bytes (current entry ou current pointer)
+} HANDLE;
 
 int T2FS_INIT = 0;
+int HANDLE_ID = 0;
+FILA2 OPEN_DIRS;
+FILA2 OPEN_FILES;
+HANDLE* CWD;
 struct t2fs_superbloco SUPERBLOCK;
 struct t2fs_inode INODE_ZERO;
 
 
+// Retorna TID sequencial
+int get_handle_id(){
+	int id = HANDLE_ID;
+	HANDLE_ID = HANDLE_ID + 1;
+	return id;
+}
+
+// Inicializacao T2FS
 int init(){
 	unsigned char buffer[SECTOR_SIZE];
 
@@ -48,7 +59,7 @@ int init(){
 
 	
 	// Imprime dados do disco e structs
-	printf("SECTOR_SIZE: %d\n",SECTOR_SIZE);
+	printf("SECTOR_SIZE: %d\n\n",SECTOR_SIZE);
 	printf("SUPERBLOCK.id: %.4s\n",SUPERBLOCK.id);
 	printf("SUPERBLOCK.version: %x\n",SUPERBLOCK.version);
 	printf("SUPERBLOCK.superblockSize: %d\n",SUPERBLOCK.superblockSize);
@@ -56,11 +67,48 @@ int init(){
 	printf("SUPERBLOCK.freeInodeBitmapSize: %d\n",SUPERBLOCK.freeInodeBitmapSize);
 	printf("SUPERBLOCK.inodeAreaSize: %d\n",SUPERBLOCK.inodeAreaSize);
 	printf("SUPERBLOCK.blockSize: %d\n",SUPERBLOCK.blockSize);
-	printf("SUPERBLOCK.diskSize: %d\n",SUPERBLOCK.diskSize); 
+	printf("SUPERBLOCK.diskSize: %d\n\n",SUPERBLOCK.diskSize); 
 	printf("sizeof(struct t2fs_record): %d\n",sizeof(struct t2fs_record));
-	printf("sizeof(struct t2fs_inode): %d\n",sizeof(struct t2fs_inode));  
+	printf("sizeof(struct t2fs_inode): %d\n\n",sizeof(struct t2fs_inode));
+	printf("INODE_ZERO.blocksFileSize: %d\n",INODE_ZERO.blocksFileSize);  
+	printf("INODE_ZERO.bytesFileSize: %d\n\n",INODE_ZERO.bytesFileSize);
 
+	// Inicializa filas de arquivos e diretorios abertos
+	if(CreateFila2(&OPEN_DIRS) != SUCCESS || CreateFila2(&OPEN_FILES) != SUCCESS){
+    	printf("init(): CreateFila2(&OPEN_DIRS) || CreateFila2(&OPEN_FILES) failed!");
+		return ERROR;
+	}
 
+	// Aloca e inicializa HANDLE para o diretorio RAIZ
+    PNODE2 nodeFila = (PNODE2)malloc(sizeof(NODE2));
+    if(nodeFila == NULL){
+		printf("init(): (PNODE2)malloc(sizeof(NODE2)) failed!");
+    	return ERROR;
+	}
+
+	HANDLE *root_handle = (HANDLE*)malloc(sizeof(HANDLE));
+    if(root_handle == NULL){
+		printf("init(): (HANDLE*)malloc(sizeof(HANDLE)) failed!");
+    	return ERROR;
+	}
+
+	nodeFila->node = root_handle;
+	if(AppendFila2(&OPEN_DIRS,nodeFila) != SUCCESS){
+		printf("init(): AppendFila2(&OPEN_DIRS,nodeFila) failed!");
+  		return ERROR;
+	}
+
+	root_handle->id = get_handle_id();
+	root_handle->record.TypeVal = TYPEVAL_DIRETORIO;
+	root_handle->record.name[0] = '/';
+	root_handle->record.name[1] = '\0';
+	root_handle->record.inodeNumber = 0;	
+	root_handle->current_offset = 0;
+
+	// Inicializa o CWD (current work dir) com o handle do diretorio RAIZ
+	CWD = root_handle;
+	
+	// Fim da inicializacao da T2FS
 	T2FS_INIT = 1;
 
 	return SUCCESS;
