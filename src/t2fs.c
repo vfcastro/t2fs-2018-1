c0128546af544970e5814017064769ed098ddb33
vfcastro <vinicius_decastro@vinicius>
2018-06-12 00:54:28 -0300

    iniciado opendir2() e impl read_block()

--- src/t2fs.c
+++ src/t2fs.c
@@ -5,9 +5,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include<unistd.h> 
+
 
 #define SUCCESS 0
 #define ERROR -1
+#define DELIMITER "/"
 
 // Struct do handle de um arquivo/diretorio aberto
 typedef struct {
@@ -24,8 +27,48 @@ HANDLE* CWD;
 struct t2fs_superbloco SUPERBLOCK;
 struct t2fs_inode INODE_ZERO;
 
+// Imprime um t2fs_record
+void print_record (struct t2fs_record *record){
+	printf("TypeVal:%u\n",record->TypeVal);
+	printf("name:%s\n",record->name);
+	printf("inodeNumber:%u\n",record->inodeNumber);
+}
+
+// Le um bloco do disco e copia para a area de memoria *buffer
+int read_block (DWORD block, unsigned char *buffer){
+	// Checa se o bloco eh maior que o tamanho do disco
+	if(block > SUPERBLOCK.diskSize-1){
+		printf("read_block(): block:%d bigger then disk size: %d!\n",block,SUPERBLOCK.diskSize-1);
+    	return ERROR;		
+	}	
+
+	// Checa se o buffer é invalido
+	if(buffer == NULL){
+		printf("read_block(): buffer is NULL!\n");
+    	return ERROR;		
+	}
+
+	// Calcula o setor inicial do bloco no disco
+	DWORD sector = block * SUPERBLOCK.blockSize;
+
+	// Le os setores do disco e grava no buffer
+	int i;
+	for(i=0; i<SUPERBLOCK.blockSize; i++){
+		if(read_sector(sector,buffer) != SUCCESS){
+			printf("read_block(): read_sector(sector=%d,buffer=%d) failed!\n",sector,(int)buffer);
+			return ERROR;
+		}
+		sector = sector + 1;
+		buffer = buffer + SECTOR_SIZE;
+	}
+	
+	// Realizou a leitura com sucesso
+	return SUCCESS;
+}
+
 // Imprime HANDLEs abertos nas filas
 void print_handles(PFILA2 fila, char* name){
+	printf("print_handles(&%s):\n",name);
 	PNODE2 nodeFila;
 	HANDLE *handle;
 	if(FirstFila2(fila) != SUCCESS)
@@ -34,8 +77,7 @@ void print_handles(PFILA2 fila, char* name){
 		do {
 			nodeFila = GetAtIteratorFila2(fila);
 			handle = nodeFila->node;
-			printf("print_handles(&%s):\nid:%d\nrecord.TypeVal:%x\nrecord.name:\"%s\"\nrecord.inodeNumber:%d\nhandle->current_offset:%d\n",
-					name,
+			printf("id:%d\nrecord.TypeVal:%x\nrecord.name:\"%s\"\nrecord.inodeNumber:%d\nhandle->current_offset:%d\n",
 					handle->id,
 					handle->record.TypeVal,
 					handle->record.name,
@@ -79,7 +121,7 @@ int init(){
 	memcpy(&INODE_ZERO,&buffer,sizeof(struct t2fs_inode));
 
 	
-	// Imprime dados do disco e structs
+	/* Imprime dados do disco e structs
 	printf("SECTOR_SIZE: %d\n\n",SECTOR_SIZE);
 	printf("SUPERBLOCK.id: %.4s\n",SUPERBLOCK.id);
 	printf("SUPERBLOCK.version: %x\n",SUPERBLOCK.version);
@@ -92,7 +134,9 @@ int init(){
 	printf("sizeof(struct t2fs_record): %d\n",sizeof(struct t2fs_record));
 	printf("sizeof(struct t2fs_inode): %d\n\n",sizeof(struct t2fs_inode));
 	printf("INODE_ZERO.blocksFileSize: %d\n",INODE_ZERO.blocksFileSize);  
-	printf("INODE_ZERO.bytesFileSize: %d\n\n",INODE_ZERO.bytesFileSize);
+	printf("INODE_ZERO.bytesFileSize: %d\n",INODE_ZERO.bytesFileSize);
+	printf("INODE_ZERO.dataPtr[0]: %d\n",INODE_ZERO.dataPtr[0]);
+	printf("INODE_ZERO.dataPtr[1]: %d\n\n",INODE_ZERO.dataPtr[1]); */
 
 	// Inicializa filas de arquivos e diretorios abertos
 	if(CreateFila2(&OPEN_DIRS) != SUCCESS || CreateFila2(&OPEN_FILES) != SUCCESS){
@@ -132,7 +176,7 @@ int init(){
 	// Fim da inicializacao da T2FS
 	T2FS_INIT = 1;
 
-	print_handles(&OPEN_DIRS,"OPEN_DIRS");
+	//print_handles(&OPEN_DIRS,"OPEN_DIRS");
 
 	return SUCCESS;
 }
@@ -161,9 +205,9 @@ DIR2 opendir2 (char *pathname){
 	    	return ERROR;		
 		}
 
-	// Checa o pathname de entrada
+	// Checa se o pathname de entrada eh invalido
 	if(pathname == NULL) {
-       	printf("opendir2() failed! *pathname is NULL!\n");
+       	printf("opendir2() failed! pathname is NULL!\n");
     	return ERROR;		
 	}
 	if(strlen(pathname) < 1) {
@@ -171,16 +215,76 @@ DIR2 opendir2 (char *pathname){
     	return ERROR;		
 	}
 
-	// Entrada de diretorio atual no caminhamento do pathname
-	
+	// Se o pathname for ABSOLUTO:
+	char* token;
+	if(pathname[0] == '/'){
+		
+		// Aloca string "path" para manipulacao via strtok
+		char *path = (char*)malloc(strlen(pathname)+1);
+		strcpy(path,pathname);
+		token = strtok(path,DELIMITER);
 
-	// Checa se o pathname e relativo ou absoluto
-	if(pathname[0] == '/'){}
+		// Se o path eh o "/", abre o diretorio RAIZ e retorna o handle
+		if(token == NULL) {
+			// Aloca e inicializa HANDLE para o diretorio RAIZ
+			PNODE2 nodeFila = (PNODE2)malloc(sizeof(NODE2));
+			if(nodeFila == NULL){
+				printf("opendir2(): (PNODE2)malloc(sizeof(NODE2)) failed!");
+				return ERROR;
+			}
+
+			HANDLE *root_handle = (HANDLE*)malloc(sizeof(HANDLE));
+			if(root_handle == NULL){
+				printf("opendir2(): (HANDLE*)malloc(sizeof(HANDLE)) failed!");
+				return ERROR;
+			}
+
+			nodeFila->node = root_handle;
+			if(AppendFila2(&OPEN_DIRS,nodeFila) != SUCCESS){
+				printf("opendir2(): AppendFila2(&OPEN_DIRS,nodeFila) failed!");
+		  		return ERROR;
+			}
+
+			root_handle->id = get_handle_id();
+			root_handle->record.TypeVal = TYPEVAL_DIRETORIO;
+			root_handle->record.name[0] = '/';
+			root_handle->record.name[1] = '\0';
+			root_handle->record.inodeNumber = 0;	
+			root_handle->current_offset = 0;
+			
+			print_handles(&OPEN_DIRS,"OPEN_DIRS");
+			return root_handle->id;
+		}
+
+		// Pathname contem pelo menos um nivel a partir do diretorio raiz
+		else {
+		// Procura pelo arquivo com o nome do token encontrado no diretorio raiz 
+
+/*			unsigned char buffer[SECTOR_SIZE*SUPERBLOCK.blockSize];
+			if(read_block(67,buffer) == ERROR){
+				printf("read_block(67,buffer) == ERROR");
+				return ERROR;
+			}
+			print_record((struct t2fs_record*)buffer);  */
+
+			do{
+				printf("%s\n",token);
+				token = strtok(NULL,DELIMITER);
+			}
+			while(token != NULL);
+
+
+		}
+
+
+	}
+
+	// Se o pathname for RELATIVO:
+
+		
 
 
 
-
-	
 	return SUCCESS;
 }
 
@@ -201,7 +305,7 @@ int getcwd2 (char *pathname, int size){
 	// Checa se o tamanho do buffer *pathname eh menor que o nome do CWD
 	// CWD->record.name + 1 por causa do terminador de string
 	if(strlen(CWD->record.name)+1 > size){
-       	printf("getcwd2(): *pathname size is too small! CWD size is %d bytes, %d bytes buffer given.\n",strlen(CWD->record.name)+1,size);
+       	printf("getcwd2(): *pathname size is too small! CWD size is %d bytes, but %d bytes buffer given.\n",strlen(CWD->record.name)+1,size);
     	return ERROR;
 	}
 
---
+++ teste/opendir2.c
@@ -0,0 +1,43 @@
+/*
+	Testa a função opendir2
+
+	TESTE1: pathname vazio, retorna erro.
+	TESTE2: pathname "/", abre diretorio raiz e retorna handle
+	TESTE3: pathanem "/bla/", abre diretorio "bla" e retorna sucesso
+
+*/
+
+
+#include "../include/t2fs.h"
+#include <stdio.h>
+
+int main(){
+	char *path;
+	int handle;
+
+	// TESTE1
+	path = "";
+	handle = opendir2(path);
+	if(handle >= 0)
+		printf("TESTE1: opendir2(\"%s\"), handle: %d\n",path,handle);
+	else
+		printf("TESTE1: opendir2(\"%s\") falhou!\n",path);
+
+	// TESTE2
+	path = "/";
+	handle = opendir2(path);
+	if(handle >= 0)
+		printf("TESTE2: opendir2(\"%s\"), handle: %d\n",path,handle);
+	else
+		printf("TESTE2: opendir2(\"%s\") falhou!\n",path);
+    
+	// TESTE3
+	path = "/a/b////c////d";
+	handle = opendir2(path);
+	if(handle >= 0)
+		printf("TESTE3: opendir2(\"%s\"), handle: %d\n",path,handle);
+	else
+		printf("TESTE3: opendir2(\"%s\") falhou!\n",path);
+
+    return 0;
+}
