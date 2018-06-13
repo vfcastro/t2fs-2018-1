6d0e0b5dc872522a3346e7e7e6051b63f590804f
vfcastro <vinicius_decastro@vinicius>
2018-06-12 23:30:57 -0300

    commit final antes da entrega imcompleta

--- teste/opendir2.c
+++ teste/opendir2.c
@@ -3,8 +3,8 @@
 
 	TESTE1: pathname vazio, retorna erro.
 	TESTE2: pathname "/", abre diretorio raiz e retorna handle
-	TESTE3: pathname "/naoexiste/", abre diretorio "existe" e retorna sucesso
-
+	TESTE3: pathname "/dir1/", abre diretorio "dir1" e retorna sucesso
+	TESTE4: pathname ".././../.././dir2", abre diretorio e retorna sucesso
 
 */
 
@@ -33,12 +33,25 @@ int main(){
 		printf("TESTE2: opendir2(\"%s\") falhou!\n",path);
     
 	// TESTE3
-	path = "/nao/existe";
+	path = "/dir1";
 	handle = opendir2(path);
 	if(handle >= 0)
 		printf("TESTE3: opendir2(\"%s\"), handle: %d\n",path,handle);
 	else
 		printf("TESTE3: opendir2(\"%s\") falhou!\n",path);
+
+	// TESTE4
+	path = ".././../.././dir2";
+	handle = opendir2(path);
+	if(handle >= 0)
+		printf("TESTE4: opendir2(\"%s\"), handle: %d\n",path,handle);
+	else
+		printf("TESTE4: opendir2(\"%s\") falhou!\n",path);
+
+	
+	path = "/dir2";
+	chdir2(path);
+
 
     return 0;
 }
